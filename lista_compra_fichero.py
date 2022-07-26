1d0bf31dfddebf9c90ead760d4fb25308ec53f94
Andreu Seguí <andreuseguisegura@gmail.com>
2022-07-26 10:55:40 +0200

    26-07-2022-Cargar_lista_compra en main

--- lista_compra_fichero.py
+++ lista_compra_fichero.py
@@ -1,4 +1,5 @@
 SALIDA = "SALIR"
+ARCHIVO = "prueba_compra"
 
 
 def pregutar():
@@ -13,17 +14,25 @@ def guardar_a_lista(lista_compra, item_a_guardar):
 
 
 def archivo(lista_compra):
-    name = input("Como quieres que se llame el archivo: ")
-    with open(name + ".txt", "w") as doc:
+    with open(ARCHIVO + ".txt", "w") as doc:
         doc.write("\n".join(lista_compra))
 
 
 def main():
     lista_compra = []
+    cargarlista = input("¿Quieres recuperar la lista de la compra [S/N]? ")
+    if cargarlista == "S" or cargarlista == "s":
+        try:
+            with open(ARCHIVO + ".txt", "r") as doc:
+                lista_compra = doc.read().split("\n")
+        except FileNotFoundError:
+            print("No se ha encontrado el archivo")
+    else:
+        pass
     input_user = pregutar()
     while input_user != SALIDA:
         guardar_a_lista(lista_compra, input_user)
-        print("Tu lista de la compra es:\n{}".format("\n".join(lista_compra).capitalize()))
+        print("Tu lista de la compra es:\n{}".format("\n".join(lista_compra)))
         input_user = pregutar()
     archivo(lista_compra)
 
